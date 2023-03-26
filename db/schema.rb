f538c6c35fadea46bfc74c79d3c8cadb43a24f8e
Renee Mumbi <1723renee@gmail.com>
2023-03-26 16:55:47 +0300

    run migrations

---
+++ db/schema.rb
@@ -0,0 +1,40 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[7.0].define(version: 2023_03_26_134554) do
+  create_table "hero_powers", force: :cascade do |t|
+    t.string "strength"
+    t.integer "power_id", null: false
+    t.integer "hero_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["hero_id"], name: "index_hero_powers_on_hero_id"
+    t.index ["power_id"], name: "index_hero_powers_on_power_id"
+  end
+
+  create_table "heros", force: :cascade do |t|
+    t.string "name"
+    t.string "super_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "powers", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "hero_powers", "heros"
+  add_foreign_key "hero_powers", "powers"
+end
