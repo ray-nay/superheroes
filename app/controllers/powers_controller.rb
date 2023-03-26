cfe558aff9230941da74580b71b604bbb520c803
Renee Mumbi <1723renee@gmail.com>
2023-03-27 00:19:43 +0300

    add methods for power controller

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -1,2 +1,16 @@
 class PowersController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+    def index 
+        powers = Power.all  
+        render json: powers
+    end 
+    def show
+        power = Power.find(params[:id])
+        render json: power
+    end
+
+    private
+    def render_not_found_response
+        render json: { error: "Power not found" }, status: :not_found
+        end
 end
