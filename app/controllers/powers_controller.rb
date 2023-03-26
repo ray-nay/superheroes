26f2e69d51415fe6d234c92de459cb6c63d742ee
Renee Mumbi <1723renee@gmail.com>
2023-03-27 00:26:37 +0300

    Add update and validation

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -1,5 +1,8 @@
 class PowersController < ApplicationController
+
     rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+    rescue_from ActiveRecord::RecordInvalid, with: :render_not_valid_response
+    
     def index 
         powers = Power.all  
         render json: powers
@@ -9,8 +12,20 @@ class PowersController < ApplicationController
         render json: power
     end
 
+    def update 
+        power = Power.find(params[:id])
+        power.update!(params_description)
+        render json: power
+
     private
     def render_not_found_response
         render json: { error: "Power not found" }, status: :not_found
         end
+
+    def params_description
+        params.permit(:name, :description)
+    end
+    def render_not_valid_response
+        render json: { error: ["validation errors"] }, status: :unprocessable_entity
+    end
 end
