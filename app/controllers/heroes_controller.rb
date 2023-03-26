3e721ad1dbe9d7b8a36208dbf749daebda63c11b
Renee Mumbi <1723renee@gmail.com>
2023-03-26 23:15:33 +0300

    Add index and show methods

--- app/controllers/heroes_controller.rb
+++ app/controllers/heroes_controller.rb
@@ -1,2 +1,11 @@
 class HeroesController < ApplicationController
+    def index 
+        heroes = Hero.all  
+        render json: heroes
+    end 
+    def show 
+        hero = Hero.find_by(id: params[:id])
+        if hero
+            render json: hero
+    end 
 end
